6ed1a6fcae44bb5e19df80dbf6a3d9295e84fba2
ponyoxa <kmsm805@gmail.com>
2023-11-05 22:02:52 +0900

    Add ESLint. Fix to constant case

--- js/app.js
+++ js/app.js
@@ -1,40 +1,46 @@
 'use strict'
-function calcIsbn() {
-    const prefix = '978';
-    const country = '4';
-    const min = 1;
-    let max = "99999999";
-    let padNum = 8;
-    let publisherSelect = document.getElementById('publisherCode');
-    if (publisherSelect.value != '') {
-        let numLen = String(publisherSelect.value).length;
-        max = max.slice(numLen);
-        padNum = max.length;
-    }
-    
-    const randomNum = Math.floor(Math.random() * (Number(max) + 1 - min)) + min;
-    
-    const isbn12 = prefix + country + publisherSelect.value + String(randomNum).padStart(Number(padNum), '0');
 
-    return isbn12 + calcCheckDigit(isbn12);
+function calcIsbn () {
+  const PREFIX = '978'
+  const COUNTRY = '4'
+  const MIN = 1
+  let max = '99999999'
+  let padNum = 8
+  const PUBLISHER_SELECT = document.getElementById('publisherCode')
+  if (PUBLISHER_SELECT.value !== '') {
+    const numLen = String(PUBLISHER_SELECT.value).length
+    max = max.slice(numLen)
+    padNum = max.length
+  }
+
+  const RANDOM_NUM = Math.floor(Math.random() * (Number(max) + 1 - MIN)) + MIN
+
+  const isbn12 =
+        PREFIX +
+        COUNTRY +
+        PUBLISHER_SELECT.value +
+        String(RANDOM_NUM).padStart(Number(padNum), '0')
+
+  return isbn12 + calcCheckDigit(isbn12)
 }
 
-function calcCheckDigit(isbn12) {
-    let sum = 0;
-    for (let i = 0; i < 12; i++) {
-        sum += (i % 2 === 0) ? parseInt(isbn12[i]) : 3 * parseInt(isbn12[i]);
-    }
+function calcCheckDigit (isbn12) {
+  let sum = 0
+  for (let i = 0; i < 12; i++) {
+    sum += i % 2 === 0 ? parseInt(isbn12[i]) : 3 * parseInt(isbn12[i])
+  }
 
-    const checkDigit = (10 - (sum % 10)) % 10;
-    return checkDigit.toString();
+  const CHECK_DIGIT = (10 - (sum % 10)) % 10
+  return CHECK_DIGIT.toString()
 }
 
-function setSearchUrl(isbn) {
-    document.getElementById('searchUrl').href = 'https://bookmeter.com/search?keyword=' + isbn;
+function setSearchUrl (isbn) {
+  document.getElementById('searchUrl').href =
+        'https://bookmeter.com/search?keyword=' + isbn
 }
 
-function getIsbn() {
-    const isbn = calcIsbn()
-    document.getElementById('isbn').innerHTML = isbn;
-    setSearchUrl(isbn);
+function getIsbn () {
+  const isbn = calcIsbn()
+  document.getElementById('isbn').innerHTML = isbn
+  setSearchUrl(isbn)
 }
